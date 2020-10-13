61e3827aaef9221247fb993febb39e10c2be7155
Tom Dingenouts <t.dingenouts@live.com>
2020-10-13 20:17:33 +0200

    Initial commit

---
+++ sketch_sep29a/sketch_sep29a.ino
@@ -0,0 +1,35 @@
+/*####
+# Project 1: Lift
+# Tom Dingenouts
+
+######*/
+
+// Pin registration
+#define p_sensor 1
+#define led_story 2
+#define led_up 3
+#define led_dn 4
+#define b_up 5
+#define b_dn 6
+
+// Shiftregister
+#define sr_data
+#define sr_clock
+#define sr_latch
+
+
+
+void setup() {
+    // put your setup code here, to run once:
+    pinMode(p_sensor, OUTPUT);
+
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+
+}
+
+void shiftOut(int data) {
+
+}
